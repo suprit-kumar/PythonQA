7536e5f9d9b8bb7d1260be67fbab8dce66318650
Supritkumar <supritk16@gmail.com>
2021-01-14 12:48:00 +0530

    python basic programme first push

---
+++ basic_programmes.py
@@ -0,0 +1,60 @@
+"""Python program to add two numbers"""
+
+# def add_two_number(a,b):
+#     return a+b
+# sum = add_two_number(10,5)
+# print('sum of two numbers -->',sum)
+
+"""Maximum of two numbers in Python"""
+# def find_maximum_btn_two_numbers(a,b):
+#     if a > b:
+#         return a
+#     else:
+#         return b
+#
+# maximum = find_maximum_btn_two_numbers(10,12)
+# print('maximum between two -->',maximum)
+
+
+"""Python Program for factorial of a number"""
+# def find_factorial(num):
+#    factorial = 1
+#    for i in range(1,num+1):
+#        factorial  = factorial *i
+#    return factorial
+#
+# fact = find_factorial(4)
+# print("Factorial of the number is -->",fact)
+
+
+"""Python Program for simple interest"""
+# Simple Interest = (P x T x R)/100
+# Where,
+# P is the principle amount
+# T is the time and
+# R is the rate
+
+# def find_simple_interest(p,t,r):
+#     simple_interest = (p*t*r)/100
+#     return simple_interest
+#
+# interest = find_simple_interest(3000,7,1)
+# print('simple interest is ->',interest)
+
+"""Python Program for compound interest"""
+
+# A = P(1 + R/100) t
+# Compound Interest = A – P
+# Where,
+# A is amount
+# P is principle amount
+# R is the rate and
+# T is the time span
+
+# def compound_interest(p,r,t):
+#     amount =p * (pow((1 + r / 100), t))
+#     ci = amount - p
+#     return ci
+#
+# ci = compound_interest(10000, 10.25, 5)
+# print('ci -->',ci)
