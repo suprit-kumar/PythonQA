46eabc11c429ebd1b33b3c816f7845ea33116a98
Supritkumar <supritk16@gmail.com>
2021-01-25 18:11:10 +0530

    new list programmers added

--- list_programmes.py
+++ list_programmes.py
@@ -1,15 +1,65 @@
 """Python program to interchange first and last elements in a list"""
 
+# def interchange1Elements(arr):
+#     size = len(arr)
+#     # Swaap array
+#     temp = arr[0]
+#     arr[0] = arr[size - 1]
+#     arr[size - 1] = temp
+#
+#     return arr
+# arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# swap = interchange1Elements(arr)
+# print('After swapping list -->',swap)
 
-def interchange1Elements(arr):
-    first_index = arr[0]
-    last_index = arr[-1]
-    arr.remove(arr[0])
-    arr.remove(arr[-1])
-    arr.insert(arr[0], last_index)
-    arr.insert(arr[-1], first_index)
-    print(arr)
+
+"""Python program to swap two elements in a list"""
+
+"""Python | Ways to find length of list"""
+
+# def find_length():
+#     test_list = [1, 4, 5, 7, 8]
+#     print("1.length of list ==>",len(test_list))
+#     count = 0
+#     for i in test_list:
+#         count+=1
+#     print("2.length of list ==>",count)
+#
+# find_length()
 
 
-arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-interchange1Elements(arr)
+"""Python | Ways to check if element exists in list"""
+
+# def check_element(arr, e):
+#     # if e in arr:
+#     #     print("1.Element exist in list")
+#     # else:
+#     #     print("2.Element not exist in list")
+#     for i in arr:
+#         if i == e:
+#             print("Exist")
+#     print("Not exist")
+# test_list = [1, 4, 5, 7, 8]
+# n = 4
+# check_element(test_list, n)
+
+
+"""Different ways to clear a list in Python"""
+
+# def clear_list():
+#     list1 = [1, 2, 3]
+#     # list1[:]=[]
+#     # list1.clear()
+#     print(list1)
+#
+# clear_list()
+
+
+"""Python | Reversing a List"""
+
+# def reverse_list():
+#     list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#     print(list1[::-1])
+#     list1.reverse()
+#     print(list1)
+# reverse_list()
